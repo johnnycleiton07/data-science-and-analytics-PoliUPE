0305c2aed3509b7a246b8f9315aaa558d193a25d
Johnny Cleiton <jcws@softex.cin.ufpe.br>
2025-11-30 20:05:56 -0300

    doc: atualizando

--- 04 - Banco de Dados Não Convencionais/atividades/tratamento_database.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("seuarquivo.csv")
-
-df["inicio_vigencia"] = df["inicio_vigencia"].dt.strftime("%Y-%m-%d")
-df["fim_vigencia"] = df["fim_vigencia"].dt.strftime("%Y-%m-%d")
